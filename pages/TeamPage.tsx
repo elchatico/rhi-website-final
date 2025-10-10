c71d00caa44478209ab9bc35842e0abe900dbad4
elchatico <gracejamhu@gmail.com>
2025-10-10 22:19:21 +0300

    image edit

--- pages/TeamPage.tsx
+++ pages/TeamPage.tsx
@@ -11,13 +11,13 @@ const teamMembers = [
   {
     name: 'George Roberts',
     role: 'Co-Founder & Program Coordinator',
-    imageUrl: '../components/Images/21.jpeg',
+    imageUrl: 'components/Images/21.jpeg',
     bio: 'Manages our school and corporate partnerships, ensuring a smooth and impactful experience for all.'
   },
   {
     name: 'Lyndah Nyangasi',
     role: 'Operations Manager',
-    imageUrl: '../components/Images/3.jpeg',
+    imageUrl: 'components/Images/3.jpeg',
     bio: 'Keeps our programs running smoothly and efficiently, handling logistics and coordination.'
   },
   {
